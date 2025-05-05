3539c4d91177432d61274cc89b02540f5fa55ad0
Shu Yan <shuyan.hoong@ncs.com.sg>
2025-05-05 13:06:03 +0800

    fix ec2 terraform module

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "this" {
-  for_each            = { for instance in var.ec2_instances : instance.instance_name => instance }
-  ami                 = each.value.ami_id
-  instance_type       = each.value.instance_type
-  key_name            = each.value.key_name
-  subnet_id           = var.subnet_ids_map[each.value.subnet_name]
-  security_groups      = [for sgrp in each.value.security_groups : var.security_group_ids_map[sgrp]] 
+  for_each                    = { for instance in var.ec2_instances : instance.instance_name => instance }
+  ami                         = each.value.ami_id
+  instance_type               = each.value.instance_type
+  key_name                    = each.value.key_name
+  subnet_id                   = var.subnet_ids_map[each.value.subnet_name]
+  vpc_security_group_ids      = [for sgrp in each.value.security_groups : var.security_group_ids_map[sgrp]] 
   associate_public_ip_address = true
 
   tags = {
